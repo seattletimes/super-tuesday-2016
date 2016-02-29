ac0340cd06201ed3be60ccb9e59959f96a48c5e2
Thomas Wilburn <news@thomaswilburn.net>
2016-02-29 15:45:35 -0800

    Test with previous results

--- tasks/elex.js
+++ tasks/elex.js
@@ -2,11 +2,15 @@ var exec = require("shelljs").exec;
 
 module.exports = function(grunt) {
 
-  grunt.registerTask("elex", function() {
+  grunt.registerTask("elex", function(date) {
+
+    var cmd = 'elex results "$date" --results-level state -o json > ./data/results.json'
+
+    cmd = cmd.replace("$date", date || "2016-03-01");
 
     var done = this.async();
 
-    exec('elex results "2016-03-01" --results-level state -o json > ./data/results.json', { silent: true }, done);
+    exec(cmd, { silent: true }, done);
 
   });
 
